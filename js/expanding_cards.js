8d47e7970351afc137a267ad5ca0f20f81096c6f
eloylopez <eloylopez22@gmail.com>
2022-10-25 09:57:20 -0500

    Modificacion 3

--- js/expanding_cards.js
+++ js/expanding_cards.js
@@ -5,3 +5,8 @@ panels.forEach(panel=>{
         panel.classList.add('active')
     })
 });
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
